23c44a2f4b90c721e15101b557be6e9731a37c51
Paulo Antonio Steck <pauloantoniosampaio@gmail.com>
2022-06-04 15:53:59 -0300

    Atualizando o projeto

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ public class App {
   
     public static void main(String[] args){
       
-        System.out.println("Rodando o Sistema");  
+        System.out.println("Rodando o Sistema");
+        System.out.println("Aplicação esta se conectando ao banco de dados");
     }
    }
