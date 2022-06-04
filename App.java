5ac50a6c0a108f1cb249af4b1bf4be947f376fb3
Paulo A. Steck Sampaio <106884628+PauloASteckS@users.noreply.github.com>
2022-06-04 15:47:23 -0300

    Criando o arquivo index do projeto

--- App.java
+++ App.java
@@ -2,8 +2,6 @@ public class App {
   
     public static void main(String[] args){
       
-        System.out.println("Rodando o Sistema");
-      
+        System.out.println("Rodando o Sistema");  
     }
-  
    }
